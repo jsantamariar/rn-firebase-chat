1bd665beed54efcbc361c2712465f2e058600b9e
“jsantamariar” <santamaria.web.dev@gmail.com>
2024-01-28 12:16:41 -0500

    refactor: adding prompt to create group name

--- app/(auth)/groups/index.tsx
+++ app/(auth)/groups/index.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Alert } from 'react-native';
 import { Href, Link, useRouter } from 'expo-router';
 import { addDoc, collection, getDoc, onSnapshot, orderBy, query, serverTimestamp } from 'firebase/firestore';
 import Spinner from "react-native-loading-spinner-overlay";
 import { Ionicons } from '@expo/vector-icons';
 import { firestoreDB } from '@/config/FirebaseConfig';
 import { useAuth } from '@/context/AuthContext';
+import Skeleton from '@/components/Skeleton';
 
 interface Group {
     id: string;
@@ -19,27 +20,44 @@ const groups = () => {
     const { user } = useAuth();
 
     const [loading, setLoading] = useState(false);
+    const [loadingGroups, setLoadingGroups] = useState(false);
     const [groups, setGroups] = useState<Group[]>([]);
 
     const handleCreateGroups = async () => {
-        setLoading(true);
-        try {
-            const doc = await addDoc(collection(firestoreDB, "groups"), {
-                name: `Group ${Math.floor(Math.random() * 100) + 1}`,
-                description: `This was created by ${user?.email}`,
-                createdAt: serverTimestamp(),
-            });
-            const document = await getDoc(doc);
-            router.push(`/groups/${document.id}`);
-
-        } catch (error) {
-            console.log(error);
-        } finally {
-            setLoading(false);
-        }
+        Alert.prompt("Create a new group", "Enter a new group name", [
+            {
+                text: 'Cancel',
+                style: 'cancel',
+            },
+            {
+                text: "Create Group",
+                onPress: async (inputGroupName) => {
+                    if (!inputGroupName) {
+                        alert("You must enter a group name!");
+                        return;
+                    }
+                    try {
+                        setLoading(true);
+                        const doc = await addDoc(collection(firestoreDB, "groups"), {
+                            name: inputGroupName,
+                            description: `This was created by ${user?.email}`,
+                            createdAt: serverTimestamp(),
+                        });
+                        const document = await getDoc(doc);
+                        router.push(`/groups/${document.id}`);
+                    } catch (error: any) {
+                        console.error(error);
+                        alert("Error creating group: " + error.message);
+                    } finally {
+                        setLoading(false);
+                    }
+                }
+            }
+        ]);
     };
 
     useEffect(() => {
+        setLoadingGroups(true);
         const groupsCollection = collection(firestoreDB, "groups");
         const q = query(groupsCollection, orderBy("createdAt", "asc"));
 
@@ -48,8 +66,9 @@ const groups = () => {
                 return { id: doc.id, ...doc.data() }
             });
             setGroups(newGroups as Group[]);
-        });
+            setLoadingGroups(false);
 
+        });
         return unsubscribe;
     }, []);
 
@@ -57,16 +76,34 @@ const groups = () => {
         <>
             <View style={styles.container}>
                 <Spinner visible={loading} />
-                <ScrollView>
-                    {groups.map((group) => (
-                        <Link href={`/groups/${group.id}` as Href<string>} key={group.id} asChild>
-                            <TouchableOpacity style={styles.groupCard}>
-                                <Text style={styles.groupName}>{group.name}</Text>
-                                <Text style={styles.groupDescription}>{group.description}</Text>
-                            </TouchableOpacity>
-                        </Link>
-                    ))}
-                </ScrollView>
+
+                {loadingGroups && (
+                    <ScrollView>
+                        {[...new Array(8)].map((_, index) => (
+                            <Skeleton key={index} />
+                        ))}
+                    </ScrollView>
+                )}
+
+                {!loadingGroups && groups.length === 0 && (
+                    <View style={styles.notChatsContainer}>
+                        <Text style={styles.notChatsText}>You haven't started any chat yet.</Text>
+                        <Text style={styles.notChatsText}> Click on the plus button to start one</Text>
+                    </View>
+                )}
+
+                {(!loading || !loadingGroups) && groups.length > 0 && (
+                    <ScrollView>
+                        {groups.map((group) => (
+                            <Link href={`/groups/${group.id}` as Href<string>} key={group.id} asChild>
+                                <TouchableOpacity style={styles.groupCard}>
+                                    <Text style={styles.groupName}>{group.name}</Text>
+                                    <Text style={styles.groupDescription}>{group.description}</Text>
+                                </TouchableOpacity>
+                            </Link>
+                        ))}
+                    </ScrollView>
+                )}
                 <TouchableOpacity style={styles.fab} onPress={handleCreateGroups}>
                     <Ionicons name="add" size={24} color="white" />
                 </TouchableOpacity>
@@ -87,12 +124,12 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         right: 20,
         bottom: 20,
-        backgroundColor: "#03A9F4",
+        backgroundColor: "#000",
         borderRadius: 30,
         elevation: 8,
     },
     groupCard: {
-        marginVertical: 5,
+        marginVertical: 2,
         padding: 15,
         backgroundColor: "#fff"
     },
@@ -102,6 +139,16 @@ const styles = StyleSheet.create({
     },
     groupDescription: {
         fontSize: 16,
+    },
+    notChatsContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        paddingHorizontal: 50,
+        gap: 5
+    },
+    notChatsText: {
+        textAlign: 'center',
     },
 });
 
