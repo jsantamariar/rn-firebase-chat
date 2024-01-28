e5dffadc94ad99457e6685d6965611dfbff19a59
“jsantamariar” <santamaria.web.dev@gmail.com>
2024-01-28 12:16:01 -0500

    refactor: adding prompt to profile screen

--- app/(auth)/profile.tsx
+++ app/(auth)/profile.tsx
@@ -1,50 +1,101 @@
-import React, { useEffect, useState } from 'react'
-import { View, StyleSheet, TextInput, Button } from 'react-native'
-import { useNavigation } from 'expo-router';
-import Spinner from "react-native-loading-spinner-overlay"
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Text, Alert, ScrollView } from 'react-native';
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import { Ionicons } from '@expo/vector-icons';
 import { firestoreDB } from '@/config/FirebaseConfig';
 import { useAuth } from '@/context/AuthContext';
+import Skeleton from '@/components/Skeleton';
+
+interface UserInfo {
+    email?: string;
+    username?: string;
+};
 
 const profile = () => {
-    const navigation = useNavigation<any>();
     const { user } = useAuth();
-    const [username, setUsername] = useState('');
+    const [userInfo, setUserInfo] = useState<UserInfo>({ email: "", username: "" })
     const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        loadUserInfo();
-    }, []);
-
     const loadUserInfo = async () => {
-        const userDocument = await getDoc(doc(firestoreDB, "users", user?.uid as string));
-
-        if (userDocument.exists()) {
-            setUsername(userDocument.data().username);
-        }
-    };
-
-    const handleUpdateUsername = async () => {
+        setLoading(true);
         try {
-            setLoading(true);
-            await updateDoc(doc(firestoreDB, "users", user?.uid as string), {
-                username,
-            });
-            navigation.goBack();
+            const userDocument = await getDoc(doc(firestoreDB, "users", user?.uid as string));
+
+            if (userDocument.exists()) {
+                setUserInfo({ email: userDocument.data().email, username: userDocument.data().username });
+            }
         } catch (error) {
-            console.log("There was an error: " + error);
+            console.log(error)
         } finally {
             setLoading(false);
         }
     };
 
+    const handleUpdateUsername = async () => {
+        Alert.prompt("Update your username", "Enter a new username", [
+            {
+                text: 'Cancel',
+                style: 'cancel',
+            },
+            {
+                text: "Update",
+                onPress: async (newUsername) => {
+                    if (!newUsername) {
+                        alert("You must enter a new usernamer!");
+                        return;
+                    }
+                    try {
+                        setLoading(true);
+                        await updateDoc(doc(firestoreDB, "users", user?.uid as string), {
+                            username: newUsername,
+                        });
+                        await loadUserInfo();
+                    } catch (error: any) {
+                        console.error(error);
+                        alert("Error creating group: " + error.message);
+                    } finally {
+                        setLoading(false);
+                    }
+                }
+            }
+        ]);
+    };
+
+    useEffect(() => {
+        loadUserInfo();
+    }, []);
+
+
     return (
         <View style={styles.container}>
-            <Spinner visible={loading} />
-            <View style={styles.card}>
-                <TextInput style={styles.textInput} value={username} onChangeText={setUsername} />
-                <Button title="Update username" onPress={handleUpdateUsername} />
-            </View>
+            {loading && (
+                <ScrollView>
+                    {[...new Array(1)].map((_, index) => (
+                        <Skeleton key={index} />
+                    ))}
+                </ScrollView>
+            )}
+            {!loading && (
+                <View style={styles.card}>
+                    <View style={styles.cardLabelContainer}>
+                        <Text style={styles.label}>
+                            Username:
+                        </Text>
+                        <Text style={styles.userInfo}>
+                            {userInfo.username}
+                        </Text>
+                        <Ionicons onPress={handleUpdateUsername} name="create" size={24} color="black" />
+                    </View>
+                    <View style={styles.cardLabelContainer}>
+                        <Text style={styles.label}>
+                            Email:
+                        </Text>
+                        <Text style={styles.userInfo}>
+                            {userInfo.email}
+                        </Text>
+                    </View>
+                </View>
+            )}
         </View>
     );
 };
@@ -52,9 +103,7 @@ const profile = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 30,
+        padding: 15
     },
     card: {
         marginBottom: 20,
@@ -67,6 +116,11 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.2,
         elevation: 1,
     },
+    cardLabelContainer: {
+        flexDirection: 'row',
+        gap: 5,
+        alignItems: 'center',
+    },
     textInput: {
         height: 40,
         margin: 12,
@@ -75,7 +129,13 @@ const styles = StyleSheet.create({
         borderRadius: 9,
         padding: 10
     },
-
+    label: {
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+    userInfo: {
+        fontSize: 16
+    }
 });
 
 export default profile
